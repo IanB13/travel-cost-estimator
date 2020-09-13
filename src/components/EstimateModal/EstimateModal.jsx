35b62fad88ae108bba67568701f0d06a541c2aaa
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-09-13 13:40:45 +0100

    Added Icons

--- src/components/EstimateModal/EstimateModal.jsx
+++ src/components/EstimateModal/EstimateModal.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
-import { Button, Modal, Menu  } from 'semantic-ui-react'
+import { Button, Modal, Menu ,Image } from 'semantic-ui-react'
 import DistEstimateForm from "./DistEstimateForm"
+import road from "../../resources/road.svg" //Need to credit
+import clock from "../../resources/clock.svg"
+import crow from "../../resources/crow.svg"
+
 
 const EstimateModal = () => {
     const [open, setOpen] = useState(false)
@@ -26,17 +30,29 @@ const EstimateModal = () => {
                         active = {activeMenu === 'Trip Distance'}
                         name='Trip Distance'
                         onClick={handleMenuClick}
-                    />
+                    > 
+                    <Image src = {road} alt="road" avatar/>
+                        Trip Distance
+                    </Menu.Item>
+
                     <Menu.Item
-                        active = {activeMenu === 'Trip Time'}
+                        active={activeMenu === 'Trip Time'}
                         name='Trip Time'
                         onClick={handleMenuClick}
-                    />
+                    >
+                    <Image src={clock} alt="clock" avatar />
+                        Trip Time<span> </span>
+                    </Menu.Item>
+
                     <Menu.Item
-                        active = {activeMenu === 'Crow Flies Distance'}
+                        active={activeMenu === 'Crow Flies Distance'}
                         name='Crow Flies Distance'
                         onClick={handleMenuClick}
-                    />
+                    >
+                    <Image src={crow} alt="crow" avatar />
+                        Crow Flies Distance
+                    </Menu.Item>
+
                 </Menu>
         
                 <DistEstimateForm activeMenu = {activeMenu} setOpen = {setOpen}/>
