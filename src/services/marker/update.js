945629d47c77273b10bfe166e097940b40b0145e
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-09-15 09:21:54 +0100

    Added route drawing

--- src/services/marker/update.js
+++ src/services/marker/update.js
@@ -27,13 +27,21 @@ const findMilesVarRate = (travel, varRate) => {
 
     }
 }
+//end helper functions
+
 
 //code for deleting and re-writing info window 
 const updateMarkers = ({google,estimate,jobs}) =>{
-    
+    //direction services
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(google.map); 
+
    for(const job of jobs){
        google.maps.event.clearInstanceListeners(job.marker);
-       // pull into function
+
+        
+
+       // turn switch case into seperate function
        let content = "error occured, please refresh"
 
        switch(estimate.tripType){
@@ -68,8 +76,29 @@ const updateMarkers = ({google,estimate,jobs}) =>{
        const clientInfoWindow = new google.maps.InfoWindow({
            content
        });
+       
+
        job.marker.addListener('click', () => {
            clientInfoWindow.open(google.map, job.marker);
+
+            if(estimate.tripType === "Crow Flies Distance"){
+           const crowFlies = new google.maps.Polyline({
+               path:[
+                   job.travel.directions.request.origin.location,
+                   job.position
+               ],
+               geodesic: true,
+               strokeColor: "#2185d0",
+               strokeOpacity: 1.0,
+               strokeWeight: 2               
+           })
+           crowFlies.setMap(google.map)
+        }
+        else{
+            // renders directions
+           directionsRenderer.setDirections(job.travel.directions);
+        }
+
        });
    }
 }
