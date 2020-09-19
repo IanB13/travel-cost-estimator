65348db545c4905333a667335ad10f561a8f5482
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-09-19 21:37:20 +0100

    Added basic instructions

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Map from './components/Map'; 
 import EstimateModal from './components/EstimateModal/EstimateModal'
 import InfoModal from './components/InfoModal'
+import Instructions from './components/Instructions'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div className="App">
       <Map />
+      <Instructions />
       <EstimateModal />
       <InfoModal />
     </div>
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Modal, List ,Image } from 'semantic-ui-react'
+import worker from "../resources/worker.svg"
+import hammer from "../resources/hammer.svg"
+
+
+function InfoModal() {
+  const [open, setOpen] = React.useState(true)
+  const IconStyle = {
+    "height": "1.5em",
+    'paddingLeft':' 0.2em',
+    'paddingRight': '0.2em'
+}
+  return (
+    <Modal
+      closeIcon
+      open={open}
+      size = 'tiny' 
+      onClose={() => setOpen(false)}
+    >
+      <Modal.Content>
+        <List as='ol'>
+        <List.Item as='li'>Select an estimate</List.Item>
+        <List.Item as='li'>Select an estimate type then flat or variable rate </List.Item>
+        <List.Item as='li'>Click on one of the tool icons
+            <Image src = {hammer} alt="tools" inline style = {IconStyle } /> 
+            to find the estimated cost of travel from the workshop icon
+            <Image src = {worker} alt="workshop" inline style = {IconStyle} /> 
+        </List.Item>
+        </List>
+      </Modal.Content>
+    </Modal>
+  )
+}
+
+export default InfoModal
